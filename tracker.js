58e2c1adc4641e369df64b8e5d54b72806e81796
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-07-11 16:51:17 +0530

    delete method update

--- tracker.js
+++ tracker.js
@@ -17,7 +17,7 @@ document.getElementById('form').addEventListener('submit',(event)=>{
 
     let updated = JSON.stringify(expense);
 
-    axios.post('https://crudcrud.com/api/262878156ff64c8b91c7990306338d68/appointmentdata',updated,{  
+    axios.post('https://crudcrud.com/api/afe5c20e300b435c9456c4a00f8bcead/appointmentData',updated,{  
     headers: {
         'Content-Type': 'application/json' 
     }
@@ -43,13 +43,12 @@ document.getElementById('form').addEventListener('submit',(event)=>{
     document.getElementById('form').reset();
 });
 
-let list = document.getElementById('list');
+const list = document.getElementById('list');
 list.querySelector('ul').innerHTML="";
 
 window.addEventListener('DOMContentLoaded',()=>{
-    axios.get('https://crudcrud.com/api/262878156ff64c8b91c7990306338d68/appointmentdata')
+    axios.get('https://crudcrud.com/api/afe5c20e300b435c9456c4a00f8bcead/appointmentData')
     .then((response) => {for(let i=0; i<response.data.length; i++){
-        console.log(response.data[i]);
         showUser(response.data[i]);
     }
     })
@@ -59,41 +58,36 @@ window.addEventListener('DOMContentLoaded',()=>{
 function showUser(user){
     const li = document.createElement('li');
     li.classList.add('list-group-item', 'list-group-item-light', 'font-weight-bold');
-    li.innerHTML= `${user.amount} - ${user.description} - ${user.category} <button data-desc="${user.description}" class="btn btn-danger float-right py-1 delete">Delete</button><button data-desc="${user.description}" class="btn btn-primary float-right mr-3 py-1 edit">Edit</button>`
-    div.querySelector('ul').appendChild(li);
+    li.innerHTML= `${user.amount} - ${user.description} - ${user.category} <button data-id="${user._id}" class="btn btn-danger float-right py-1 delete">Delete</button><button data-id="${user._id}" class="btn btn-primary float-right mr-3 py-1 edit">Edit</button>`
+    list.querySelector('ul').appendChild(li);
 
 }
 
-let div = document.getElementById('list');
-
-for(let i=0; i<localStorage.length; i++){
-    let key = localStorage.key(i);
-    let value = localStorage.getItem(key);
-    let obj = JSON.parse(value);
-    const li = document.createElement('li');
-    li.classList.add('list-group-item', 'list-group-item-light', 'font-weight-bold');
-    li.innerHTML= `${obj.amount} - ${obj.description} - ${obj.category} <button data-desc="${obj.description}" class="btn btn-danger float-right py-1 delete">Delete</button><button data-desc="${obj.description}" class="btn btn-primary float-right mr-3 py-1 edit">Edit</button>`;
-    div.querySelector('ul').appendChild(li);
-
-}
-
-div.querySelector('ul').addEventListener('click',function(e){    
+list.querySelector('ul').addEventListener('click', function(e){
     if(e.target.classList.contains('delete')){
-        let key = e.target.getAttribute('data-desc');
+        axios.delete(`https://crudcrud.com/api/afe5c20e300b435c9456c4a00f8bcead/appointmentData/${e.target.getAttribute("data-id")}`);
         e.target.parentElement.remove();
-        localStorage.removeItem(key);        
     }
 });
 
-div.querySelector('ul').addEventListener('click',(e)=>{
-    if(e.target.classList.contains('edit')){
-        let key = e.target.getAttribute('data-desc');
-        let l_key = localStorage.getItem(key);
-        let obj = JSON.parse(l_key);
-        amt.value = obj.amount;
-        desc.value = obj.description;
-        catg.value = obj.category;
-        e.target.parentElement.remove();
-        localStorage.removeItem(key);        
-    }
-});
+
+// div.querySelector('ul').addEventListener('click',function(e){    
+//     if(e.target.classList.contains('delete')){
+//         let key = e.target.getAttribute('data-desc');
+//         e.target.parentElement.remove();
+//         localStorage.removeItem(key);        
+//     }
+// });
+
+// div.querySelector('ul').addEventListener('click',(e)=>{
+//     if(e.target.classList.contains('edit')){
+//         let key = e.target.getAttribute('data-desc');
+//         let l_key = localStorage.getItem(key);
+//         let obj = JSON.parse(l_key);
+//         amt.value = obj.amount;
+//         desc.value = obj.description;
+//         catg.value = obj.category;
+//         e.target.parentElement.remove();
+//         localStorage.removeItem(key);        
+//     }
+// });
