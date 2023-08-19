e9d13087eb3df65e3d44f0c70cc20125fc69dc13
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-08-19 15:52:02 +0530

    edit working
    with the help of frontend getting to edit data in form then update it

--- controllers/tracker.js
+++ controllers/tracker.js
@@ -1,18 +1,33 @@
-const { error } = require('console');
 const expense = require('../models/tracker');
 
 exports.create = (req, res, next) => {
     const {id, amount, category, description} = req.body;
-    expense.create({
-        amount: amount,
-        category: category,
-        description: description
-    })
-    .then(() => res.redirect('/form.html'))
-    .catch((error) => {
-        console.log('error while creating data', error);
-        res.status(500).json({error:'Internal server error'});
-    });
+    console.log(req.body);
+    if(id){
+        expense.findByPk(id)
+        .then((data) => {
+            data.amount = amount;
+            data.description = description;
+            data.category = category;
+            return data.save();
+        })
+        .then(() => res.redirect('/form.html'))
+        .catch((error) => {
+            console.log('error while creating data by id', error);
+            res.status(500).json({error:'Internal server error'});
+        });
+    }else{
+        expense.create({
+            amount: amount,
+            category: category,
+            description: description
+        })
+        .then(() => res.redirect('/form.html'))
+        .catch((error) => {
+            console.log('error while creating data', error);
+            res.status(500).json({error:'Internal server error'});
+        });
+    }
 };
 exports.delete = (req, res, next) => {
     const {delID} = req.params;
@@ -26,34 +41,14 @@ exports.delete = (req, res, next) => {
     }
 }
 
-exports.edit = (req, res, next) => {
-    const {editID} = req.params;
-    if(editID){
-        expense.findByPk(editID)
-        .then(() => {
-            res.redirect(`/form.html?editID=${editID}`);
-        })
-        .catch((error) => {
-            console.log('error while deleting data', error);
-            res.status(500).json({error:'Internal server error'});
-        });
-    }
-};
 
-// exports.getdata = (req, res, next) => {
-//     const id = req.query;
-//     console.log(id);
-//     expense.findAll()
-//     .then((data) => {
-//         res.json(data);
-//     })
-//     .catch((error) => {
-//         console.log('error while fetching data', error);
-//         res.status(500).json({error:'Internal server error'});
-//     });
-// };
-exports.getformdata = (req, res, next) => {
-    console.log('inside form data');
-    const id = req.query;
-    console.log('form id ',id);
-}
+exports.getdata = (req, res, next) => {
+    expense.findAll()
+    .then((data) => {
+        res.json(data);
+    })
+    .catch((error) => {
+        console.log('error while fetching data', error);
+        res.status(500).json({error:'Internal server error'});
+    });
+};
